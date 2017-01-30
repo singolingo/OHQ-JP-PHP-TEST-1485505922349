3a887a22a12c6599be849addc47fb1a8d26ba0b7
030144714 <030144714@SPR8701.ad.jp.omron.net>
2017-01-30 16:38:35 +0900

    1

---
+++ callback.php
@@ -0,0 +1,50 @@
+<?php
+
+$appid = $clientid = $clientsecret = $state = "";
+
+if (isset($_POST["AppID"])) {
+	$appid = $_POST[‘AppID’];
+}
+if (isset($_POST["ClientID"])) {
+	$clientid = $_POST[‘ClientID’];
+}
+if (isset($_POST["ClientSecret"])) {
+	$clientsecret = $_POST[‘ClientSecret’];
+}
+if (isset($_POST["State"])) {
+	$state = $_POST[‘State’];
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang = “ja”>
+<head>
+<meta charset = “UFT-8”>
+<title>OGSC クラウドから、コールバックで返却される値を取得。</title>
+</head>
+<body>
+<h1>コールバック先</h1>
+
+<BR>
+<?php
+echo $appid;
+?>
+<BR>
+<?php
+echo $clientid;
+?>
+<BR>
+<?php
+echo $clientsecret;
+?>
+<BR>
+<?php
+echo $state;
+?>
+<BR>
+
+
+
+</body>
+</html>
